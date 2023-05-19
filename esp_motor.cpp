d4ca0d51d7b200b98d6b32633141f7353c6bcc71
Snahal Kumar <77937488+snahal04@users.noreply.github.com>
2023-05-19 17:13:03 +0530

    Update esp_motor.cpp

--- esp_motor.cpp
+++ esp_motor.cpp
@@ -11,7 +11,7 @@
 //#include <Servo.h>   //To Control Servo motor
 
 #define MAX_DISTANCE 200
-#define MAX_SPEED 130 // sets speed of DC  motors
+unsigned int MAX_SPEED = 120; // sets speed of DC  motors
 #define MAX_SPEED_OFFSET 20
 //#define led A4
 //#define led2 A5
@@ -20,6 +20,7 @@
 #define D1 A1
 #define D2 A2
 #define D3 A3
+#define D4 A4
 // V0 = D0 Forward
 // V1 = D1 Backward
 // V2 = D2 Left
@@ -32,6 +33,7 @@ AF_DCMotor motor4(4, MOTOR34_1KHZ);
 
 boolean goesForward = false;
 boolean goesBackward = false;
+boolean goesForward_Boost = false;
 
 unsigned int speedSet = 0;
 int pos = 0;
@@ -39,12 +41,14 @@ unsigned int Fd = 0; // Variable to store Right IR sensor value:
 unsigned int Bd = 0;  // Variable to store Left IR sensor value:
 unsigned int Ld = 0;
 unsigned int Rd = 0;
+unsigned int boost = 0;
 void setup()
 {
   pinMode(D0, INPUT);
   pinMode(D1, INPUT);
   pinMode(D2, INPUT);
   pinMode(D3, INPUT);
+  pinMode(D4, INPUT);
   Serial.begin(9600); // initailize serial communication at 9600 bits per second:
   delay(2000);
 }
@@ -55,6 +59,7 @@ void loop()
   Bd = digitalRead(D1); // read the value from Right IR sensor:
   Ld = digitalRead(D2); // read the value from Right IR sensor:
   Rd = digitalRead(D3); // read the value from Right IR sensor:
+  boost = digitalRead(D4); // read the value from Right IR sensor:
   //  delay(700);
   Serial.println(Fd);
   //  delay(700);
@@ -63,30 +68,45 @@ void loop()
   Serial.println(Ld);
   //  delay(700);
   Serial.println(Rd);
-  if (Fd == 1 && Bd == 0 && Ld == 0 && Rd == 0)
+  
+  if (Fd == 1 && Bd == 0 && Ld == 0 && Rd == 0 && boost == 1)
   {
+    MAX_SPEED = 250;
+    moveForward_Boost();
+    // delay(2000);
+    Serial.println("******************FORWARD________BOOST");
+  }
+  
+  else if (Fd == 1 && Bd == 0 && Ld == 0 && Rd == 0 && boost == 0)
+  {
+    MAX_SPEED = 120;
     moveForward();
     // delay(2000);
     Serial.println("******************FORWARD");
   }
-  else if (Fd == 0 && Bd == 1 && Ld == 0 && Rd == 0)
+  
+  else if (Fd == 0 && Bd == 1 && Ld == 0 && Rd == 0 )
   {
+    
     moveBackward();
     // delay(2000);
     Serial.println("******************BACK");
   }
+  
   else if (Fd == 0 && Bd == 0 && Ld == 1 && Rd == 0)
   {
-    turnLeft(100);
+    turnLeft(20);
     Serial.println("***************** LEFT ");
     //    delay(100);
   }
+  
   else if (Fd == 0 && Bd == 0 && Ld == 0 && Rd == 1)
   {
-    turnRight(100);
+    turnRight(20);
     Serial.println("***************** RIGHT");
     //    delay(100);
   }
+  
   else if (Fd == 1 && Bd == 0 && Ld == 1 && Rd == 0) {
     turnLeft(50);
     moveForward();
@@ -128,6 +148,33 @@ void moveStop()
   motor4.run(RELEASE);
 }
 
+void moveForward_Boost()
+{
+  if (!goesForward_Boost)
+  {
+    //    T_count = 0;
+    Serial.println("!!!!!!!!!!!!     F_BOOSTED     !!!!!!!!!!!!!!");
+    //    digitalWrite(led, HIGH);
+    //    digitalWrite(led2, LOW);
+    goesForward_Boost = true;
+    goesBackward = false;
+    goesForward = false;
+    motor1.run(FORWARD);
+    motor2.run(FORWARD);
+    motor3.run(FORWARD);
+    motor4.run(FORWARD);
+    // maxspeed = 250
+    for (speedSet = 0; speedSet <= MAX_SPEED; speedSet += 5)
+    {
+      motor1.setSpeed(speedSet);
+      motor2.setSpeed(speedSet);
+      motor3.setSpeed(speedSet);
+      motor4.setSpeed(speedSet);
+      delay(5);
+    }
+  }
+}
+
 void moveForward()
 {
   if (!goesForward)
@@ -138,11 +185,12 @@ void moveForward()
     //    digitalWrite(led2, LOW);
     goesForward = true;
     goesBackward = false;
+    goesForward_Boost = false;
     motor1.run(FORWARD);
     motor2.run(FORWARD);
     motor3.run(FORWARD);
     motor4.run(FORWARD);
-    for (speedSet = 0; speedSet < MAX_SPEED; speedSet += 5)
+    for (speedSet = 0; speedSet <= MAX_SPEED; speedSet += 5)
     {
       motor1.setSpeed(speedSet);
       motor2.setSpeed(speedSet);
@@ -166,7 +214,7 @@ void moveBackward()
     motor2.run(BACKWARD);
     motor3.run(BACKWARD);
     motor4.run(BACKWARD);
-    for (speedSet = 0; speedSet < 110; speedSet += 6)
+    for (speedSet = 0; speedSet < 130; speedSet += 6)
     {
       motor1.setSpeed(speedSet);
       motor2.setSpeed(speedSet);
@@ -180,10 +228,10 @@ void moveBackward()
 
 int turnRight(int z)
 {
-  motor1.setSpeed(130);
-  motor2.setSpeed(130);
-  motor3.setSpeed(130);
-  motor4.setSpeed(130);
+  motor1.setSpeed(110);
+  motor2.setSpeed(110);
+  motor3.setSpeed(110);
+  motor4.setSpeed(110);
   Serial.println("******* RIGHT TURN ********");
   motor1.run(FORWARD);
   motor2.run(FORWARD);
@@ -198,10 +246,10 @@ int turnRight(int z)
 
 int turnLeft(int z)
 {
-  motor1.setSpeed(130);
-  motor2.setSpeed(130);
-  motor3.setSpeed(130);
-  motor4.setSpeed(130);
+  motor1.setSpeed(110);
+  motor2.setSpeed(110);
+  motor3.setSpeed(110);
+  motor4.setSpeed(110);
   Serial.println("******* LEFT TURN ********");
   motor1.run(BACKWARD);
   motor2.run(BACKWARD);
